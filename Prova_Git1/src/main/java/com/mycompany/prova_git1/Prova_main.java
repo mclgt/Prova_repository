0e1c349c6a8d433419a06811131f9ef147ab17f8
miche <miche@Computerino.station>
2024-11-14 21:20:01 +0100

    Stampa Michela

--- Prova_Git1/src/main/java/com/mycompany/prova_git1/Prova_main.java
+++ Prova_Git1/src/main/java/com/mycompany/prova_git1/Prova_main.java
@@ -15,6 +15,7 @@ public class Prova_main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Michela");
     }
     
 }
