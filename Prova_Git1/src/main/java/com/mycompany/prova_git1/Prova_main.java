47bea9d2bed9de767e1a889c5ddd302cc24775e7
miche <miche@Computerino.station>
2024-11-14 20:55:58 +0100

    Creazione Main

---
+++ Prova_Git1/src/main/java/com/mycompany/prova_git1/Prova_main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.prova_git1;
+
+/**
+ *
+ * @author miche
+ */
+public class Prova_main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
